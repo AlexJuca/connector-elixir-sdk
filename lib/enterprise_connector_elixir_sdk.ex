d0a0431c683dd6ebdbe9fa385534ad6253504fa0
Alexandre Juca <corextechnologies@gmail.com>
2020-11-14 12:42:10 +0100

    Initial commit

---
+++ lib/enterprise_connector_elixir_sdk.ex
@@ -0,0 +1,18 @@
+defmodule EnterpriseConnectorElixirSdk do
+  @moduledoc """
+  Documentation for `EnterpriseConnectorElixirSdk`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EnterpriseConnectorElixirSdk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/enterprise_connector_elixir_sdk_test.exs
@@ -0,0 +1,8 @@
+defmodule EnterpriseConnectorElixirSdkTest do
+  use ExUnit.Case
+  doctest EnterpriseConnectorElixirSdk
+
+  test "greets the world" do
+    assert EnterpriseConnectorElixirSdk.hello() == :world
+  end
+end
